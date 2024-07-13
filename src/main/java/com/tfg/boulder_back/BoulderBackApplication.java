f3e9725381c43be1b420c9697640788e0a721826
Pablo <pablovillef@gmail.com>
2024-07-13 12:09:59 +0200

    Proyecto base de Spring Initializr

---
+++ src/main/java/com/tfg/boulder_back/BoulderBackApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.boulder_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulderBackApplication.class, args);
+	}
+
+}
