11d4a4dae6dbe1c74bc1895809aaf31915f1cb1b
Pablovillef <pablovillef@gmail.com>
2024-10-13 13:30:19 +0200

    Fix/registro via (#27)
    * Adaptado el formulario de creacion de Via para controlar los posibles errores al introducir los campos.
    
    * Anadido control de errores a la creacion de vias.

--- src/main/java/com/tfg/boulder_back/entity/Route.java
+++ src/main/java/com/tfg/boulder_back/entity/Route.java
@@ -3,6 +3,7 @@ package com.tfg.boulder_back.entity;
 
 import com.tfg.boulder_back.constants.TypeRoute;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -21,19 +22,24 @@ public class Route {
     @Column(name = "id_route", nullable = false, length = 5)
     private Long id;
 
-    @Column(name = "qr", nullable = false, length = 50)
+    @NotEmpty
+    @Column(name = "qr", nullable = false, unique = true, length = 50)
     private String qrRoute;
 
+    @NotEmpty
     @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name; // TODO: pattern
 
     @Enumerated(EnumType.STRING)
+    @NotEmpty
     @Column(name = "type", nullable = false)
     private TypeRoute typeRoute;
 
+    @NotEmpty
     @Column(name = "num_nivel", nullable = false, length = 50)
     private int num_nivel; // numerico
 
+    @NotEmpty
     @Column(name = "presa", nullable = false, length = 50)
     private String presa; // Colores
 
